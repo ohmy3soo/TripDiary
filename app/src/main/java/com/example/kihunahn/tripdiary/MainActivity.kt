add22d7d03099324bfd6b9e7c015b640011e7fee
hanwjdgh <hanwjdgh@naver.com>
2018-07-06 15:29:58 +0900

    change main

--- app/src/main/java/com/example/kihunahn/tripdiary/MainActivity.kt
+++ app/src/main/java/com/example/kihunahn/tripdiary/MainActivity.kt
@@ -2,12 +2,26 @@ package com.example.kihunahn.tripdiary
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //
+
+        main_start.setOnClickListener {
+            val intent = Intent(applicationContext, MapActivity::class.java)
+            startActivity(intent)
+        }
+        main_archive.setOnClickListener {
+            val intent = Intent(applicationContext, StorageActivity::class.java)
+            startActivity(intent)
+        }
+        main_help.setOnClickListener {
+            val intent = Intent(applicationContext, MapActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
