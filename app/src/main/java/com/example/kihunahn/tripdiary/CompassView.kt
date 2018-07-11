82d43e24378fcd1a3fa1f067b5b28a6291b29109
ahn <ohmy3soo@nate.com>
2018-07-11 13:49:31 +0900

    .

--- app/src/main/java/com/example/kihunahn/tripdiary/CompassView.kt
+++ app/src/main/java/com/example/kihunahn/tripdiary/CompassView.kt
@@ -1,12 +1,35 @@
-package com.example.kihunahn.tripdiary
+package org.androidtown.tripdiary
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.view.View
+import com.example.kihunahn.tripdiary.R
 
-class CompassView : AppCompatActivity() {
+class CompassView(ctx: Context) : View(ctx) {
+    private val mCompass: Drawable
+    private var mAzimuth = 0f
+    private val PADDING = 2
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_compass_view)
+    init {
+
+        this.mCompass = ctx.resources.getDrawable(R.drawable.arrow_n)
     }
+
+    override fun onDraw(canvas: Canvas) {
+        canvas.save()
+
+        canvas.rotate(360 - mAzimuth, (PADDING + mCompass.minimumWidth / 2).toFloat(), (PADDING + mCompass.minimumHeight / 2).toFloat())
+        mCompass.setBounds(PADDING, PADDING, PADDING + mCompass.minimumWidth, PADDING + mCompass.minimumHeight)
+
+        mCompass.draw(canvas)
+        canvas.restore()
+
+        super.onDraw(canvas)
+    }
+
+    fun setAzimuth(aAzimuth: Float) {
+        mAzimuth = aAzimuth
+    }
+
 }
